5eaaba89ed687829680e1641e90257fbcef24b63
Anne Marie Taylor <annemarie.taylor@hmcts.net>
2022-02-24 20:24:08 +0000

    Added new project

---
+++ Mosh-Beginners-Course/Hello World/app.py
@@ -0,0 +1,17 @@
+print("I am Anne Marie Taylor")
+print("o-----")
+print(" ¦¦¦¦")
+print("*" * 10)
+price = 10
+rating = 4.9
+name = "amt"
+is_published = True
+price = 20
+print(price)
+patient_name = "John Smith"
+patient_age = 20
+new_patient = True
+name = input("What is your name? ")
+colour = input("What is your favourite colour? ")
+print("Hi " + name)
+print(name + " likes " + colour)
