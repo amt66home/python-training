976a37446e370e2fec4267840792b7e69d29cfb7
Anne Marie Taylor <amt66@hotmail.co.uk>
2022-03-09 07:39:44 +0000

    added more conditions

--- Games/Car.py
+++ Games/Car.py
@@ -1,10 +1,19 @@
 command = ""
+started = False
 
 while True:
     command = input("> ").lower # take the input, convert to lowercase and store in command
     if command == "start":
+        if started:
+            print('The car is already started')
+        else:
+            started == True
         print("Car started ready to go")
     elif command == "stop":
+        if not started:
+            print('The car is already stopped')
+        else:
+            started == False
         print("Car has stopped")
     elif command == "help":
         print("""
@@ -17,4 +26,3 @@ quit - to exit
 else:
     print("I don't understand that")
 
-# got to 1.37.40
